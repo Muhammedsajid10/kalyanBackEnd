083e5829da77ea0ac65cfb6f2e12e45638acac51
muhammedsajid <muhammedsajid@q2kg.onmicrosoft.com>
2025-04-21 00:27:40 +0530

    Initial backend upload

--- server.js
+++ server.js
@@ -22,7 +22,31 @@ app.use(express.json());
 
 await initialize();
 
-app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
+
+
+const allowedOrigins = [
+  "https://kalyanengineeringcorp.com/", // replace with actual frontend URL
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+
+
+
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/uploads/products", express.static(path.join("uploads/products")));
